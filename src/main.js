068cf0c4d5496bc19f1e09d68f67493e0007268b
Ria Iyer <riyer4@ucsc.edu>
2025-02-04 15:34:44 -0800

    more files added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-//yes
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    // scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config)
+
+// reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
