3c51b721a6572bbed58d5f19f5a62b6f41e8daa6
Ria Iyer <riyer4@ucsc.edu>
2025-02-04 17:54:57 -0800

    base done

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,1 +1,51 @@
-//yes
+class Menu extends Phaser.Scene {
+    constructor () {
+        super('playScene')
+    }
+
+    preload() {
+
+        //sprite + image loading
+        // ex) this.load.image('rocket', './assets/rocket.png')
+
+
+        //audio loading
+        // ex) this.load.audio('sfx-select', './assets/sfx-select.wav')
+
+    }
+
+    create() {
+
+        //main screen
+        //ex) this.mainScreen = this.add.tilesprite(0, 0, 640, 480, 'mainScreen).setOrigin(0, 0)
+
+        let menuConfig = {
+            fontFamily: 'Times New Roman',
+            fontSize: '35px',
+            backgroundColor: '#add8e6',
+            color: '#000',
+            allig: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width/2, game.config.height - 150, 'Welcome to wtvr this is', menuConfig).setOrigin(0.5)
+
+        //keys
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+
+
+
+    }
+
+    update() {
+
+        // put what happens when certain keys are pressed in this menu screen
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,1 +1,68 @@
-//yes
+class Play extends Phaser.Scene {
+    constructor () {
+        super('playScene')
+    }
+
+    create() {
+
+
+        //borders maybe
+        this.add.rectange(0, 0, game.config.width, game.config.width / 15, 0xFFFFFF).setOrigin(0, 0)
+        this.add.rectange(0, 0, game.config.width - game.config.width / 15, game.config.width, 0xFFFFFF).setOrigin(0, 0)
+        this.add.rectange(0, 0, game.config.width / 15, game.config.width, 0xFFFFFF).setOrigin(0, 0)
+        this.add.rectange(0, 0, game.config.width, game.config.width / 15, 0xFFFFFF).setOrigin(0, 0)
+
+        // add all characters / sprites
+
+        //define the keys
+
+        //keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
+        //keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+
+        // score stuff
+
+        this.p1Score = 0 // score initialization
+
+        // add the same stuff as the menu config layout
+
+        // check for if the game is over or not
+
+        this.gameOver = false
+
+        //clock stuff
+
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5)
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5)
+            this.gameOver = true
+
+        }, null, this)
+
+    }
+
+    update() {
+
+
+        //check input for restarting
+
+        // update bg tile position
+
+        // update all sprites and characters
+
+        // do a collision check
+
+        if (!this.gameOver) {
+            //update everything under here
+        }
+
+    }
+
+    checkCollision(x, y) {
+        // copy from prev assignment
+        // or take inspo from golf in class assignment
+    }
+
+
+}
